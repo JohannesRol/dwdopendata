aafb095da8375644fb6e431287bd455bfa06d4a8
JohannesRol <Johannes.Roling@haw-hamburg.de>
2019-06-05 10:14:48 +0200

    new methods:   build_tree   search_folder   ftp_set_debuglevel   str_to_timestamp  modified methods:    wind    ftp_login    __init__

--- dwdopendata.py
+++ dwdopendata.py
@@ -4,14 +4,40 @@ Date created: 2019-05-08
 Version: 0.0.1
 """
 from datetime import datetime as dt
+from datetime import timedelta
 from math import pi, acos, sin, cos
 from ftplib import FTP, all_errors
 import zipfile
-
+import urllib.request
+import os
+import json
 
 # the resolution dict should help find the resolution
 resolution = {'10 min': '10_minutes', '1 min': '1_minute', 'y': 'annual', 'd': 'daily',
               'h': 'hourly', 'm': 'monthly', 'm_y': 'multi_annual', 's_d': 'subdaily'}
+reso_folder = {'recent': [500, 1], 'now': [1, 0], 'historical': [14600, 500]}  # days
+
+
+def search_folder(key: str, unique: bool = True) -> list:
+    """ Search the dwd_tree.txt files folder for a keyword
+
+    :param key: key word of the folder name
+    :type key: str
+    :param unique: When the string should be unique
+    :type unique: bool
+    :return:
+    """
+    with open('dwd_tree.txt', 'r') as file:
+        paths = json.load(file)
+    results = list()
+    for path in paths:
+        if unique:
+            if key == path['folder']:
+                results.append(path)
+        else:
+            if key in path['folder']:
+                results.append(path)
+    return results
 
 
 class Location:
@@ -25,6 +51,9 @@ class Location:
         self.coordinate = [lat, lon]
         self.server = 'opendata.dwd.de'
         self.cdc_obDE_climate = 'climate_environment/CDC/observations_germany/climate/'
+        if not os.path.isfile('dwd_tree.txt'):
+            self.build_tree()
+        self.debug_level = 0
 
     def __str__(self):
         return 'Latitude: ' + str(self.coordinate[0]) + ', Longitude: ' + str(self.coordinate[1])
@@ -59,18 +88,40 @@ class Location:
 
         return data
 
-    def wind(self, timestamp_start, timestamp_end, reso='10_minutes'):
+    def wind(self, start, end, reso='10_minutes', where='cdc_obDE_climate'):
         if reso in resolution:
             reso = resolution[reso]
+        if where == 'cdc_obDE_climate':
+            where = self.cdc_obDE_climate
+        start, end = self.str_to_timestamp(start, end)
         wind = 'wind'
-        path = self.cdc_obDE_climate + reso + '/' + wind
-        print(path)
-        return path
-        # with open(path, 'r') as file:
-        #    data = file.readlines()
-        # file.close()
-        # stations = self.build_station_list(data)
-        # return stations
+        wind_paths = search_folder(wind)
+        for wp in wind_paths:
+            if reso in wp['path'] and where in wp['path']:
+                path = wp['path']
+        ftp = self.ftp_login()
+        ftp.cwd(path)
+        # checks in which directorys the fitting data is
+        time_matrix = dict()
+        now = dt.now()
+        for folder in ftp.nlst():
+            time_matrix.update({folder: [False, False]})
+            if folder in reso_folder:
+                before, after = reso_folder[folder]
+                before = now - timedelta(before)
+                after = now - timedelta(after)
+                if before < start < after:
+                    time_matrix[folder][0] = True
+                if before < end < after:
+                    time_matrix[folder][1] = True
+        # todo get the station list of the affected folder
+        for key in time_matrix:
+            if True in time_matrix[key]:
+                ftp.cwd(key)
+                # download st
+                ftp.cwd('..')
+        ftp.close()
+        return time_matrix
 
     def solar(self, reso='10_Minutes'):
         if reso in resolution:
@@ -86,43 +137,176 @@ class Location:
         return stations
 
     def ftp_login(self):
+        """Handles the login to the server.
+        :return: FTP object
+        """
         try:
             ftp = FTP(self.server)
+            ftp.set_debuglevel(self.debug_level)
             ftp.login()
+            return ftp
         except all_errors as e:
             error_code_string = str(e).split(None, 1)[0]
             print(error_code_string)
-        return ftp
 
-    def explore_ftp(self, c_ftp, key):
+    def ftp_set_debuglevel(self, level):
+        """Setter for the debugging level of the ftp server
+
+        :param level: Debugging level for teh ftp client
+        :return:
+        """
+        self.debug_level = level
+
+    def ftp_explore(self, c_ftp, key):
+        """Takes to long, abandon function
+
+        :param c_ftp:
+        :param key:
+        :return:
+        """
         search_list = list()
         for dire in c_ftp.nlst():
             if '.' in dire:
                 pass
             else:
-                if key in dire:
+                if key == dire:
                     print(dire)
                     search_list.append(c_ftp.pwd())
-                    return search_list
                 if dire:
-                    c_ftp.cwd(dire)
+                    try:
+                        c_ftp.cwd(dire)
+                    except:
+                        return
                     search_list.append(self.explore_ftp(c_ftp, key))
                     print(dire)
                     c_ftp.cwd('..')
-        return search_list
+        return search_list if search_list else None
+
+    def build_tree(self):
+        """ Builds a list of dict with the path and a the name of the folder and saves it as a .txt file
+
+        **Example of the .txt**
+        First element will be the url
+
+        [
+        {"path": "https://opendata.dwd.de", "folder": "https://opendata.dwd.de"},
+        {"path": "https://opendata.dwd.de/climate/", "folder": "climate"}
+        ]
+
+        :return: True if succeeds
+        """
+        try:
+            # todo download a list of strings and don't write it to a file
+            url = 'https://' + self.server + '/weather/tree.html'
+            urllib.request.urlretrieve(url, 'dwd_tree.txt')  # download the tree
+            with open('dwd_tree.txt', 'r') as file:
+                tree = file.readlines()
+        # todo specify the exceptions
+        except:
+            print('Check your internet connection')
+            return False
+        paths = list()
+        i = len(tree) - 1
+        ident_start = 'href='
+        ident_end = '</a><br>'
+        while i != -1:
+            deleted = False
+            # delete the element when the key word is not in the string
+            if ident_start not in tree[i]:
+                del tree[i]
+                deleted = True
+            if not deleted:
+                # find the string and the folder
+                slice_start = tree[i].find(ident_start) + len(ident_start) + 1
+                slice_end = tree[i].find(ident_end)
+                path, folder = tree[i][slice_start:slice_end].split(sep='"')
+                path = path.replace('https://opendata.dwd.de/', '')
+                paths.append({'path': path, 'folder': folder[1:]})
+            i -= 1
+        paths.reverse()
+        try:
+            # save to txt file
+            with open('dwd_tree.txt', 'w') as f:
+                json.dump(paths, f)
+        # todo specify the exceptions
+        except:
+            print("Saving the output wasn't successful")
+            return False
+        return True
+
+    @staticmethod
+    def str_to_timestamp(start: str, end: str):
+        """Returns a datetime object
+
+        :param start: ISO Format timestamp string with 'T' as separator
+        :param end: ISO Format timestamp string with 'T' as separator
+
+        **Example:**
+        Date of the example: 2018-03-02
+        str_to_timestamp('2000-12-31T10:24', '2000-12-31T11:24'))
+
+        :returns: (datetime, datetime)
+        """
+        dati_form = {'time': {2: '%H', 4: '%H%M', 6: '%H%M%S'},
+                     'date': {6: '%y%m%d', 8: '%Y%m%d'}
+                     }
+
+        def is_numeric(value):
+            try:
+                float(value)
+                return True
+            except ValueError:
+                return False
+
+        def drop_char(string):
+            tmp = ''
+            for c in string:
+                if is_numeric(c):
+                    tmp = tmp + c
+            return tmp
+
+        start = start.replace(' ', 'T')
+        end = end.replace(' ', 'T')
+        length = len(start)
+        try:
+            if length != len(end):
+                print('Start and end date are not in the same format')
+                raise SyntaxError
+            if ('T' not in start or 'T' not in end) and length > 10:
+                print('There have to be a "T" or a space between the date and the time')
+                raise SyntaxError
+        except SyntaxError as fail:
+            print(fail)
+            print('There are several formats supported, please use one of them.')
+            print(dati_form)
+            return None
+        try:
+            if 'T' in start:
+                date, time = start.split('T')
+                time = drop_char(time)
+                time_format = dati_form['time'][len(time)]
+            else:
+                date = start
+                time_format = ''
+            date = drop_char(date)
+            date_format = dati_form['date'][len(date)]
+            datetime_format = date_format + time_format
+
+            start = drop_char(start)
+            end = drop_char(end)
+
+            timestamp_start = dt.strptime(start, datetime_format)
+            timestamp_end = dt.strptime(end, datetime_format)
+        except KeyError:
+            print("Check your timestamps. Here an Example: '2019-01/01T00', '2019/02$01T00'")
+            return None
+
+        if timestamp_start > timestamp_end:
+            timestamp_start = timestamp_end
+            timestamp_end = dt.strptime(start, datetime_format)
+
+        return timestamp_start, timestamp_end
 
 
-def unzip():
-    _dt_format = '%Y%m%d%H%M'
-    ipath = '/Users/Johnny/Documents/repo/dwdopendata/10minutenwerte_ff_00003_19930428_19991231_hist.zip'
-    with zipfile.ZipFile(ipath, "r") as zip_ref:
-        with zip_ref.open(zip_ref.namelist()[0], 'r') as myfile:
-            data = myfile.readlines()
-    data = [data_point.decode().strip().split(';') for data_point in data]
-    for i in range(1, len(data)):
-        data[i][0] = data[i][0].strip()
-        data[i][1] = dt.strptime(data[i][1].strip(), _dt_format)
-        data[i][2] = float(data[i][2].strip())
-        data[i][3] = float(data[i][3].strip())
-        data[i][4] = float(data[i][4].strip())
-    return data
+
+
